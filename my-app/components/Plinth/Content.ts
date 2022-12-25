96984e5e36aeb9f397a08f60d5024f6738e77c87
Astha Singh <asthasingh1729@gmail.com>
2022-12-25 16:56:29 +0530

    done

--- my-app/components/Plinth/Content.ts
+++ my-app/components/Plinth/Content.ts
@@ -3,25 +3,25 @@ const events = [
     title: 'CP DUELS',
     content:
       'CP DUEL is an in-person 1v1 knockout tournament. As the meaning of “duel” suggests this will be a “coding combat” between 2 individuals i.e both the participants will be given the same problem and the first person to solve it wins. There will be M problems of varying difficulty levels and M winners for each problem. Participants will be given a problem based on their CF Rating. Each duel will have a fixed time.',
-    link: 'https://docs.google.com/forms/d/1G1-G8-LgZAFjFr-NpC0ZiginWnZZ0ZOfj-EtfQRHGWI/edit?ts=63a71ea7',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSdLHc7PArUSnDiZbSU7RKa5PtuL376AZV2p3VYkcbWZQihaZQ/viewform?usp=sf_link',
   },
   {
     title: 'C PEAK',
     content:
       'C PEAK is a tournament in which contestants are eliminated on the basis of their “problem solving speed”.  In each round of this tournament half the candidates will be eliminated. There will be 10 rounds (2 online + 8 on-site). Each round will take 20-25 minutes with a buffer of 5-15 minutes for the next round.',
-    link: 'https://docs.google.com/forms/d/1lDaC830rHUMO07kGxtU5eHEIUL_2SkB54MCTMLIPPXU/edit?ts=63a71f44',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLScyBNYmg_JE_cXgOFUNWobtDqshR1wAlIXZuq73Uz2HUyi6jQ/viewform?usp=sf_link',
   },
   {
     title: 'IUPC',
     content:
       'This is an on-site “teams-only” contest. The contest will be ICPC style with a 10 minute penalty for each wrong submission and will follow the ICPC format. The preferable number of people in a team is 3.',
-    link: 'https://docs.google.com/forms/d/1_7MlOuf7YS6rUOFJYbqVps_hwOkjIJVMLgZGjhipknE/edit?ts=63a71c9d',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSe4cE-hQrO0pQRaGb5LxHkLjm5_XXlInrN36BFmu1Bnd5Ggug/viewform?usp=sf_link',
   },
   {
     title: 'ENIGMA',
     content:
       'This is Cybros’ flagship rated contest. It is a normal one-person competitive programming contest. It has been hosted on esteemed competitive programming websites like Codechef and Codeforces in the previous editions of Plinth. ',
-    link: 'https://docs.google.com/forms/d/1nmaDkb26KjehlR8lYoCGOtryignaxa7CGXCZU5X69Ws/edit?ts=63a71b88',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSeETAUcVCXd0iytIqI4xQkN-c3Rbkw0ayDXrTSYZed2YCVmkQ/viewform?usp=sf_link',
   },
 ];
 export default events;
