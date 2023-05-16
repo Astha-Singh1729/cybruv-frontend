24cc5639a4c329e55e17b84aa245b649ef8d3d85
Astha Singh <asthasingh1729@gmail.com>
2023-05-16 08:28:11 +0530

    checking whether just push on github update site at vercel as well or not

---
+++ my-app/pages/pod.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/home/Navbar"
+export default function Home() {
+    return (
+        <>
+            <Navbar></Navbar>
+            problem of the day
+        </>
+    )
+}
